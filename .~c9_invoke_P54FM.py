30430e2c762a367f5f6a88c2e8acfc30b8421763
Rachit Agarwal <rachitagarwal0202@gmail.com>
2017-02-19 02:04:24 +0000

    forms

--- .~c9_invoke_P54FM.py
+++ .~c9_invoke_P54FM.py
@@ -18,25 +18,16 @@ def main():
 def stream():
     return render_template('stream.html')
 
-@app.route('/bias', methods = ['POST', 'GET'])
+@app.route('/bias')
 def bias():
-    if request.method == 'POST':
-        article_url = request.form["article_url"]
-        article_info = ""
-        #article_info = processURL(article_url)
-        return render_template('bias.html', article_info = article_info)
     return render_template('bias.html')
 
-
-
-@app.route('/search', methods = ['POST', 'GET'])
+@app.route('/search')
 def search():
-    if request.method == 'POST':
-        query = request.form["search_query"]
-        articles_info=""
-        #articles_info = manualSearch(query)
-        return render_template('search.html', articles_info = articles_info)
     return render_template('search.html')
+    
+
+
 
 
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
--- static/js/stream.js
+++ static/js/stream.js
@@ -1,12 +1,12 @@
 Cesium.BingMapsApi.defaultKey = "rAT6FD3QWEAHqFKTyVOH~PGug46gl7KWuYW5EmoKrhA~An9_0N3tOAo3VQ-1JgMBj3hOgSBb1-610evZpUQVW48gUc62JQ9RjNzRKSWBPLqI";
 
 var viewer = new Cesium.Viewer('cesiumContainer');
-  viewer.dataSources.add(Cesium.GeoJsonDataSource.load('static/js/cesium/Apps/SampleData/countries.geo.json', {
+  viewer.dataSources.add(Cesium.GeoJsonDataSource.load('static/js/cesium/Apps/SampleData/template.json', {
         stroke: Cesium.Color.BLACK,
         fill: Cesium.Color.BLUE.withAlpha(0.2),
         strokeWidth: 0
     }));
-   var promise = Cesium.GeoJsonDataSource.load('static/js/cesium/Apps/SampleData/countries.geo.json');
+   var promise = Cesium.GeoJsonDataSource.load('static/js/cesium/Apps/SampleData/template.json');
     promise.then(function(dataSource) {
         viewer.dataSources.add(dataSource);
         
